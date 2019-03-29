5da29249e1000b60f0dd3de19c5f8bf3dd8a98f1
sgaron-cse <sgaron.cse@gmail.com>
2019-03-29 15:28:31 -0400

    Trying to get Kibana APM to work in flask

--- al_ui/app.py
+++ al_ui/app.py
@@ -1,5 +1,6 @@
 import logging
 
+from elasticapm.contrib.flask import ElasticAPM
 from flask import Flask
 from flask.logging import default_handler
 
@@ -45,7 +46,7 @@ register_site_specific_routes = context.register_site_specific_routes
 
 ##########################
 # App settings
-app = Flask("alui")
+app = Flask("al_ui")
 app.logger.setLevel(60)  # This completely turns off the flask logger
 app.config.update(
     SESSION_COOKIE_SECURE=True,
@@ -103,6 +104,13 @@ def main():
         from werkzeug.contrib.profiler import ProfilerMiddleware
         app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[30])
     app.jinja_env.cache = {}
+
+    if config.config.core.metrics.apm_server.server_url is not None:
+        apm = ElasticAPM(logging=True)
+        apm.init_app(app, server_url=config.config.core.metrics.apm_server.server_url,
+                         service_name="al_ui")
+        apm.capture_message("AL UI Connected!")
+
     app.run(host="0.0.0.0", debug=False)
 
 
