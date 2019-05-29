40ed0b8a304da85fd65595f78e31e5afed3a0dda
sgaron-cse <sgaron.cse@gmail.com>
2019-05-29 13:46:39 -0400

    Better way to create test data

--- test/base.py
+++ test/base.py
@@ -10,6 +10,7 @@ import warnings
 
 from assemblyline.common.security import get_password_hash
 from assemblyline.common.yara import YaraImporter
+from assemblyline.odm.models.emptyresult import EmptyResult
 from assemblyline.odm.models.error import Error
 from assemblyline.odm.models.file import File
 from assemblyline.odm.models.result import Result
@@ -88,7 +89,7 @@ def create_signatures():
     return [p['rule']['name'] for p in parsed]
 
 
-def create_users(ds):
+def create_users(ds, log=None):
     user_data = User({
         "agrees_with_tos": "NOW",
         "classification": "RESTRICTED",
@@ -98,13 +99,19 @@ def create_users(ds):
         "is_admin": True})
     ds.user.save('admin', user_data)
     ds.user_settings.save('admin', UserSettings())
+    if log:
+        log.info(f"\tU:{user_data.uname}   P:{user_data.uname}")
+
     user_data = User({"name": "user", "password": get_password_hash("user"), "uname": "user"})
     ds.user.save('user', user_data)
     ds.user_settings.save('user', UserSettings())
+    if log:
+        log.info(f"\tU:{user_data.uname}   P:{user_data.uname}")
+
     ds.user.commit()
 
 
-def create_services(ds):
+def create_services(ds, log=None):
     for svc_name, svc in SERVICES.items():
         service_data = Service({
             "name": svc_name,
@@ -122,11 +129,13 @@ def create_services(ds):
 
         # Save the default delta entry
         ds.service_delta.save(service_data.name, {"version": service_data.version})
+        if log:
+            log.info(f'\t{svc_name}')
     ds.service_delta.commit()
     ds.service.commit()
 
 
-def create_errors_for_file(ds, f, services_done):
+def create_errors_for_file(ds, f, services_done, log=None):
     e_list = []
     for _ in range(random.randint(0, 1)):
         e = random_model_obj(Error)
@@ -141,17 +150,18 @@ def create_errors_for_file(ds, f, services_done):
 
         e_key = e.build_key()
         e_list.append(e_key)
+        if log:
+            log.info(f"\t\t\t{e_key}")
         ds.error.save(e_key, e)
 
     return e_list
 
 
-def create_results_for_file(ds, f, possible_childs=None):
+def create_results_for_file(ds, f, possible_childs=None, log=None):
     r_list = []
     services_done = []
     for _ in range(random.randint(2, 5)):
         r = random_model_obj(Result)
-
         # Only one result per service per file
         while r.response.service_name in services_done:
             r.response.service_name = random.choice(list(SERVICES.keys()))
@@ -160,33 +170,46 @@ def create_results_for_file(ds, f, possible_childs=None):
         # Set the sha256
         r.sha256 = f
 
-        # Set random extracted files that are not top level
-        if not possible_childs:
-            r.response.extracted = []
+        if random.randint(1, 10) > 8:
+            # Generate and empty result
+            r_key = f"{r.build_key()}.e"
+            ds.emptyresult.save(r_key, random_model_obj(EmptyResult))
         else:
-            for e in r.response.extracted:
-                e.sha256 = random.choice(possible_childs)
+            r_key = r.build_key()
+            # Set random extracted files that are not top level
+            if not possible_childs:
+                r.response.extracted = []
+            else:
+                for e in r.response.extracted:
+                    e.sha256 = random.choice(possible_childs)
 
-        # Set random supplementary files that are not top level
-        if not possible_childs:
-            r.response.supplementary = []
-        else:
-            for s in r.response.supplementary:
-                s.sha256 = random.choice(possible_childs)
+            # Set random supplementary files that are not top level
+            if not possible_childs:
+                r.response.supplementary = []
+            else:
+                for s in r.response.supplementary:
+                    s.sha256 = random.choice(possible_childs)
+            ds.result.save(r_key, r)
 
-        r_key = r.build_key()
+
+        if log:
+            log.info(f"\t\t\t{r_key}")
         r_list.append(r_key)
-        ds.result.save(r_key, r)
 
     return r_list
 
 
-def create_submission(ds, fs):
+def create_submission(ds, fs, log=None):
     f_list = []
     r_list = []
     e_list = []
 
     first_level_files = []
+    s = random_model_obj(Submission)
+
+    if log:
+        log.info(f"\t{s.sid}")
+        log.info(f"\tGenerating files for submission...")
     for _ in range(random.randint(4, 8)):
         f = random_model_obj(File)
         byte_str = get_random_phrase(wmin=8, wmax=20).encode()
@@ -194,20 +217,26 @@ def create_submission(ds, fs):
         f.sha256 = sha256
         ds.file.save(sha256, f)
         fs.put(sha256, byte_str)
+
+        if log:
+            log.info(f"\t\t{sha256}")
+
         f_list.append(sha256)
 
     for _ in range(random.randint(1, 2)):
         first_level_files.append(f_list.pop())
 
+    if log:
+        log.info(f"\t\tGenerating results and errors for top level files...")
     for f in first_level_files:
-        r_list.extend(create_results_for_file(ds, f, possible_childs=f_list))
-        e_list.extend(create_errors_for_file(ds, f, [x.split('.')[1] for x in r_list if x.startswith(f)]))
+        r_list.extend(create_results_for_file(ds, f, possible_childs=f_list, log=log))
+        e_list.extend(create_errors_for_file(ds, f, [x.split('.')[1] for x in r_list if x.startswith(f)], log=log))
 
+    if log:
+        log.info(f"\t\tGenerating results and errors for children files...")
     for f in f_list:
-        r_list.extend(create_results_for_file(ds, f))
-        e_list.extend(create_errors_for_file(ds, f, [x.split('.')[1] for x in r_list if x.startswith(f)]))
-
-    s = random_model_obj(Submission)
+        r_list.extend(create_results_for_file(ds, f, log=log))
+        e_list.extend(create_errors_for_file(ds, f, [x.split('.')[1] for x in r_list if x.startswith(f)], log=log))
 
     s.results = r_list
     s.errors = e_list
--- test/create_test_data.py
+++ test/create_test_data.py
@@ -1,21 +1,13 @@
 import sys
 import random
-import os.path
 
 from assemblyline.common import forge
-from assemblyline.common.security import get_password_hash
-from assemblyline.common.yara import YaraImporter
 from assemblyline.odm.models.alert import Alert
-from assemblyline.odm.models.emptyresult import EmptyResult
-from assemblyline.odm.models.error import Error
-from assemblyline.odm.models.file import File
 from assemblyline.odm.models.heuristic import Heuristic
-from assemblyline.odm.models.result import Result
-from assemblyline.odm.models.service import Service
-from assemblyline.odm.models.submission import Submission
-from assemblyline.odm.models.user import User
-from assemblyline.odm.models.user_settings import UserSettings
-from assemblyline.odm.randomizer import random_model_obj, SERVICES, get_random_phrase
+from assemblyline.odm.randomizer import random_model_obj, get_random_phrase
+
+# noinspection PyUnresolvedReferences
+from base import create_users, create_services, create_signatures, create_submission
 
 
 class PrintLogger(object):
@@ -35,54 +27,22 @@ class PrintLogger(object):
 def create_basic_data(log, ds=None):
     ds = ds or forge.get_datastore()
     if ds.user.search("id:*", rows=0)['total'] == 0:
-        log.info("\nCreating user object...")
-        user_data = User({
-            "agrees_with_tos": "NOW",
-            "classification": "RESTRICTED",
-            "name": "Admin user",
-            "password": get_password_hash("admin"),
-            "uname": "admin",
-            "is_admin": True})
-        ds.user.save('admin', user_data)
-        ds.user_settings.save('admin', UserSettings())
-        log.info(f"\tU:{user_data.uname}   P:{user_data.uname}")
-        user_data = User({"name": "user", "password": get_password_hash("user"), "uname": "user"})
-        ds.user.save('user', user_data)
-        ds.user_settings.save('user', UserSettings())
-        log.info(f"\tU:{user_data.uname}    P:{user_data.uname}")
+        log.info("\nCreating user objects...")
+        create_users(ds, log=log)
     else:
         log.info("\nUsers already exist, skipping...")
 
     if ds.service_delta.search("id:*", rows=0)['total'] == 0:
         log.info("\nCreating services...")
-        for svc_name, svc in SERVICES.items():
-            service_data = Service({
-                "name": svc_name,
-                "enabled": True,
-                "category": svc[0],
-                "stage": svc[1],
-                "version": "3.3.0"
-            })
-            # Save a v3 service
-            ds.service.save(f"{service_data.name}_{service_data.version}", service_data)
-
-            # Save the same service as v4
-            service_data.version = "4.0.0"
-            ds.service.save(f"{service_data.name}_{service_data.version}", service_data)
-
-            # Save the default delta entry
-            ds.service_delta.save(service_data.name, {"version": service_data.version})
-            log.info(f'\t{svc_name}')
+        create_services(ds, log=log)
     else:
         log.info("Services already exist, skipping...")
 
     if ds.signature.search("id:*", rows=0)['total'] == 0:
         log.info("\nImporting test signatures...")
-        yp = YaraImporter(logger=PrintLogger(indent="\t"))
-        path = os.path.abspath(os.path.dirname(__file__))
-        parsed = yp.parse_file(os.path.join(path, 'al_yara_signatures.yar'))
-        yp.import_now([p['rule'] for p in parsed])
-        signatures = [p['rule']['name'] for p in parsed]
+        signatures = create_signatures()
+        for s in signatures:
+            log.info(f"\t{s}")
     else:
         log.info("Signatures already exist, skipping...")
         signatures = list(ds.signature.keys())
@@ -104,91 +64,18 @@ def create_extra_data(log, signatures, ds=None, fs=None):
     ds = ds or forge.get_datastore()
     fs = fs or forge.get_filestore()
 
-    log.info("\nCreating 20 Files...")
-    file_hashes = []
-    for x in range(20):
-        f = random_model_obj(File)
-        file_hashes.append(f.sha256)
-        ds.file.save(f.sha256, f)
-
-        fs.put(f.sha256, f.sha256)
-
-        log.info(f"\t{f.sha256}")
-
-    log.info("\nCreating 6 Results per file...")
-    result_keys = []
-    for fh in file_hashes:
-        other_files = list(set(file_hashes) - {fh})
-
-        for x in range(6):
-            r = random_model_obj(Result)
-            r.sha256 = fh
-
-            for ext in r.response.extracted:
-                ext.sha256 = random.choice(other_files)
-                if fh == ext.sha256:
-                    raise Exception("Invalid extracted file")
-
-            for supp in r.response.supplementary:
-                supp.sha256 = random.choice(other_files)
-                if fh == supp.sha256:
-                    raise Exception("Invalid supplementary file")
-
-            for tag in r.result.tags:
-                if random.randint(0, 3) == 1:
-                    tag.value = random.choice(signatures)
-                    tag.type = "FILE_YARA_RULE"
-
-            key = r.build_key()
-            result_keys.append(key)
-            ds.result.save(key, r)
-            log.info(f"\t{key}")
-
-    log.info("\nCreating 4 EmptyResults per file...")
-    for fh in file_hashes:
-        for x in range(4):
-            # Get a random result key
-            r = random_model_obj(Result)
-            r.sha256 = fh
-            key = f"{r.build_key()}.e"
-            result_keys.append(key)
-
-            # Save an empty result using that key
-            ds.emptyresult.save(key, random_model_obj(EmptyResult))
-            log.info(f"\t{key}")
-
-    log.info("\nCreating 2 Errors per file...")
-    error_keys = []
-    for fh in file_hashes:
-        for x in range(2):
-            e = random_model_obj(Error)
-            e.sha256 = fh
-            key = e.build_key()
-            error_keys.append(key)
-            ds.error.save(key, e)
-            log.info(f"\t{key}")
-
     log.info("\nCreating 10 Submissions...")
     submissions = []
     for x in range(10):
-        s = random_model_obj(Submission)
-        s.results = random.choices(result_keys, k=random.randint(5, 15))
-        s.errors = random.choices(error_keys, k=random.randint(0, 3))
-        s_file_hashes = list(set([x[:64] for x in s.results]).union(set([x[:64] for x in s.errors])))
-        s.error_count = len(s.errors)
-        s.file_count = len(s_file_hashes)
-        for f in s.files:
-            f.sha256 = random.choice(s_file_hashes)
-        ds.submission.save(s.sid, s)
-        submissions.append({"sid": s.sid, "file": s.files[0].sha256})
-        log.info(f"\t{s.sid}")
+        s = create_submission(ds, fs, log=log)
+        submissions.append(s)
 
     log.info("\nCreating 50 Alerts...")
     for x in range(50):
         submission = random.choice(submissions)
         a = random_model_obj(Alert)
-        a.file.sha256 = submission['file']
-        a.sid = submission['sid']
+        a.file.sha256 = submission.files[0].sha256
+        a.sid = submission.sid
         a.owner = random.choice(['admin', 'user', 'other', None])
         ds.alert.save(a.alert_id, a)
         log.info(f"\t{a.alert_id}")
