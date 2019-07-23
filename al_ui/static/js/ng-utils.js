0c77cebf8f0ef4689c108e47cb8c446bbcfdb1d1
sgaron-cse <sgaron.cse@gmail.com>
2019-07-23 12:22:09 -0400

    Show file size units properly

--- al_ui/static/js/ng-utils.js
+++ al_ui/static/js/ng-utils.js
@@ -752,6 +752,34 @@ utils.filter('breakableStr', function () {
     }
 });
 
+utils.filter('fileSize', function () {
+    return function (size) {
+        if (isNaN(size))
+            size = 0;
+
+        if (size < 1024)
+            return size + ' Bytes';
+
+        size /= 1024;
+
+        if (size < 1024)
+            return size.toFixed(2) + ' Kb';
+
+        size /= 1024;
+
+        if (size < 1024)
+            return size.toFixed(2) + ' Mb';
+
+        size /= 1024;
+
+        if (size < 1024)
+            return size.toFixed(2) + ' Gb';
+
+        size /= 1024;
+
+        return size.toFixed(2) + ' Tb';
+    };
+});
 
 utils.filter('floatStr', function () {
     return function (float_let) {
