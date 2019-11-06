f30b9caf1390b1e5fae2c2697ba28f6c35af779a
sgaron-cse <sgaron.cse@gmail.com>
2019-11-06 13:30:50 -0500

    Add highlighting of malicious and suspicious items to the report

--- assemblyline_ui/api/v4/submission.py
+++ assemblyline_ui/api/v4/submission.py
@@ -796,9 +796,9 @@ def get_report(submission_id, **kwargs):
             for cat in item['categories']:
 
                 submission['attack_matrix'].setdefault(cat, {})
-                submission['attack_matrix'][cat].setdefault(item['name'], [])
+                submission['attack_matrix'][cat].setdefault(item['name'], {'h_type': item['h_type'], 'files': []})
                 for name in name_map.get(sha256, [sha256]):
-                    submission['attack_matrix'][cat][item['name']].append((name, sha256))
+                    submission['attack_matrix'][cat][item['name']]['files'].append((name, sha256))
 
         # Process heuristics
         for h_type, items in heuristics.items():
@@ -828,9 +828,9 @@ def get_report(submission_id, **kwargs):
             # Tags
             submission['tags'].setdefault(summary_type, {})
             submission['tags'][summary_type].setdefault(t['type'], {})
-            submission['tags'][summary_type][t['type']].setdefault(t['value'], [])
+            submission['tags'][summary_type][t['type']].setdefault(t['value'], {'h_type': t['h_type'], 'files': []})
             for name in name_map.get(sha256, [sha256]):
-                submission['tags'][summary_type][t['type']][t['value']].append((name, sha256))
+                submission['tags'][summary_type][t['type']][t['value']]['files'].append((name, sha256))
 
         submission["file_info"] = STORAGE.file.get(submission['files'][0]['sha256'], as_obj=False)
 
