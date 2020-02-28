d496ab6cc84fcef0367eac1b83cd2cdc0e63b355
sgaron-cse <sgaron.cse@gmail.com>
2020-02-27 20:53:05 -0500

    Small patch in profile parser

--- assemblyline_ui/helper/oauth.py
+++ assemblyline_ui/helper/oauth.py
@@ -3,16 +3,17 @@ import requests
 
 
 def parse_profile(profile):
+    email_adr = profile.get('email', profile.get('upn', None))
     user = dict(
-        uname=profile.get('unique_name', profile.get('email', None)),
+        uname=profile.get('uname', email_adr),
         name=profile.get('name', None),
-        email=profile.get('email', profile.get('upn', None)),
-        password="__OAUTH2__"
+        email=email_adr,
+        password="__NO_PASSWORD__"
     )
 
     picture = profile.get('picture', None)
     if picture:
-        if picture.startswith('https://'):
+        if picture.startswith('https://') or picture.startswith('http://'):
             resp = requests.get(picture)
             if resp.ok and resp.headers.get("content-type") is not None:
                 b64_img = base64.b64encode(resp.content).decode()
