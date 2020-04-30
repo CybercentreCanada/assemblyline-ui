853770d58679122e3889da054e3f46de7d789b2c
Steve Garon <steve.garon@cyber.gc.ca>
2020-04-29 22:15:50 -0400

    Fix APIs for the new signature and attack formats

--- assemblyline_ui/api/v4/file.py
+++ assemblyline_ui/api/v4/file.py
@@ -507,8 +507,8 @@ def get_file_results(sha256, **kwargs):
                         pass
 
                     # Process Attack matrix
-                    attack_id = sec['heuristic'].get('attack_id', None)
-                    if attack_id:
+                    for attack in sec['heuristic'].get('attack', []):
+                        attack_id = attack['attack_id']
                         attack_pattern_def = attack_map.get(attack_id, {})
                         if attack_pattern_def:
                             for cat in attack_pattern_def['categories']:
@@ -521,10 +521,11 @@ def get_file_results(sha256, **kwargs):
                             pass
 
                     # Process Signatures
-                    if sec['heuristic'].get('signature', None):
-                        sig = (sec['heuristic'].get('signature', None), h_type)
-                        if sig not in output['signatures']:
-                            output['signatures'].add(sig)
+                    for signature in sec['heuristic'].get('signature', []):
+                        if signature['name'] != 'null':
+                            sig = (signature['name'], h_type)
+                            if sig not in output['signatures']:
+                                output['signatures'].add(sig)
 
                 # Process tags
                 for t in sec['tags']:
