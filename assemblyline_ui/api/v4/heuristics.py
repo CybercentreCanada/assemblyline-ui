d51989e980c2418d15809ef65c94dfd95c05e806
Steve Garon <steve.garon@cyber.gc.ca>
2020-06-17 12:12:37 -0400

    Calculate statistics via a separate process and save it as a cache entry so the UI show the results faster.

--- assemblyline_ui/api/v4/heuristics.py
+++ assemblyline_ui/api/v4/heuristics.py
@@ -13,31 +13,6 @@ heuristics_api = make_subapi_blueprint(SUB_API, api_version=4)
 heuristics_api._doc = "View the different heuristics of the system"
 
 
-def get_stat_for_heuristic(p_id, p_name, p_classification):
-    stats = STORAGE.result.stats("result.score",
-                                 query=f"result.sections.heuristic.heur_id:{p_id}")
-    if stats['count'] == 0:
-        return {
-            'heur_id': p_id,
-            'name': p_name,
-            'classification': p_classification,
-            'count': stats['count'],
-            'min': 0,
-            'max': 0,
-            'avg': 0,
-        }
-    else:
-        return {
-            'heur_id': p_id,
-            'name': p_name,
-            'classification': p_classification,
-            'count': stats['count'],
-            'min': int(stats['min']),
-            'max': int(stats['max']),
-            'avg': int(stats['avg']),
-        }
-
-
 @heuristics_api.route("/<heuristic_id>/", methods=["GET"])
 @api_login(allow_readonly=False, required_priv=["R"])
 def get_heuristic(heuristic_id, **kwargs):
@@ -67,7 +42,7 @@ def get_heuristic(heuristic_id, **kwargs):
         return make_api_response("", "Heuristic not found", 404)
 
     if user and Classification.is_accessible(user['classification'], h['classification']):
-        h.update(get_stat_for_heuristic(h['heur_id'], h['name'], h['classification']))
+        h.update(STORAGE.get_stat_for_heuristic(h['heur_id'], h['name'], h['classification']))
         return make_api_response(h)
     else:
         return make_api_response("", "You are not allowed to see this heuristic...", 403)
@@ -99,12 +74,7 @@ def heuritics_statistics(**kwargs):
 
     user = kwargs['user']
 
-    heur_list = sorted([(x['heur_id'], x['name'], x['classification'])
-                       for x in STORAGE.heuristic.stream_search("heur_id:*", fl="heur_id,name,classification",
-                                                                access_control=user['access_control'], as_obj=False)])
+    stats = forge.get_statistics_cache().get('heuristics') or []
 
-    with concurrent.futures.ThreadPoolExecutor(max(min(len(heur_list), 20), 1)) as executor:
-        res = [executor.submit(get_stat_for_heuristic, heur_id, name, classification)
-               for heur_id, name, classification in heur_list]
-
-    return make_api_response(sorted([r.result() for r in res], key=lambda i: i['heur_id']))
+    return make_api_response([x for x in stats
+                              if Classification.is_accessible(user['classification'], x['classification'])])
--- assemblyline_ui/api/v4/signature.py
+++ assemblyline_ui/api/v4/signature.py
@@ -1,5 +1,3 @@
-import concurrent.futures
-
 from flask import request
 from hashlib import sha256
 
@@ -638,47 +636,12 @@ def signature_statistics(**kwargs):
       },
      ...
     ]"""
-
     user = kwargs['user']
 
-    def get_stat_for_signature(p_id, p_source, p_name, p_type, p_classification):
-        stats = STORAGE.result.stats("result.score",
-                                     query=f'result.sections.tags.file.rule.{p_type}:"{p_source}.{p_name}"')
-        if stats['count'] == 0:
-            return {
-                'id': p_id,
-                'source': p_source,
-                'name': p_name,
-                'type': p_type,
-                'classification': p_classification,
-                'count': stats['count'],
-                'min': 0,
-                'max': 0,
-                'avg': 0,
-            }
-        else:
-            return {
-                'id': p_id,
-                'source': p_source,
-                'name': p_name,
-                'type': p_type,
-                'classification': p_classification,
-                'count': stats['count'],
-                'min': int(stats['min']),
-                'max': int(stats['max']),
-                'avg': int(stats['avg']),
-            }
+    stats = forge.get_statistics_cache().get('signatures') or []
 
-    sig_list = sorted([(x['id'], x['source'], x['name'], x['type'], x['classification'])
-                       for x in STORAGE.signature.stream_search("name:*",
-                                                                fl="id,name,type,source,classification",
-                                                                access_control=user['access_control'], as_obj=False)])
-
-    with concurrent.futures.ThreadPoolExecutor(max(min(len(sig_list), 20), 1)) as executor:
-        res = [executor.submit(get_stat_for_signature, sid, source, name, sig_type, classification)
-               for sid, source, name, sig_type, classification in sig_list]
-
-    return make_api_response(sorted([r.result() for r in res], key=lambda i: i['type']))
+    return make_api_response([x for x in stats
+                              if Classification.is_accessible(user['classification'], x['classification'])])
 
 
 @signature_api.route("/update_available/", methods=["GET"])
