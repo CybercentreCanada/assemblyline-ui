7610aa6672705b1ef9a0d27f1809d322b98f44a2
Steve Garon <steve.garon@cyber.gc.ca>
2021-03-08 10:20:41 -0500

    Remove all UI components and make this repo an API and SocketIO server only

--- assemblyline_ui/error.py
+++ assemblyline_ui/error.py
@@ -1,21 +1,15 @@
 
-import os
-
-from flask import Blueprint, render_template, request, redirect, session as flsk_session, current_app
+from flask import Blueprint, request, session as flsk_session
 from sys import exc_info
 from traceback import format_tb
-from urllib.parse import quote
-
 from werkzeug.exceptions import Forbidden, Unauthorized
 
 from assemblyline_ui.api.base import make_api_response
 from assemblyline_ui.config import AUDIT, AUDIT_LOG, LOGGER, config, KV_SESSION
-from assemblyline_ui.helper.views import redirect_helper
 from assemblyline_ui.http_exceptions import AccessDeniedException, AuthenticationException
 from assemblyline_ui.logger import log_with_traceback
 
 errors = Blueprint("errors", __name__)
-AL_NEXT_ONLY = os.environ.get('AL_NEXT_ONLY', 'false').lower() == 'true'
 
 
 ######################################
@@ -23,10 +17,7 @@ AL_NEXT_ONLY = os.environ.get('AL_NEXT_ONLY', 'false').lower() == 'true'
 @errors.app_errorhandler(400)
 def handle_400(e):
     error_message = str(e)
-    if request.path.startswith("/api/"):
-        return make_api_response("", error_message, 400)
-    else:
-        return render_template('400.html'), 400
+    return make_api_response("", error_message, 400)
 
 
 @errors.app_errorhandler(401)
@@ -36,20 +27,14 @@ def handle_401(e):
     else:
         msg = str(e)
 
-    if request.path.startswith("/api/"):
-        data = {
-            "oauth_providers": [name for name, p in config.auth.oauth.providers.items()
-                                if p['client_id'] and p['client_secret']],
-            "allow_userpass_login": config.auth.ldap.enabled or config.auth.internal.enabled,
-            "allow_signup": config.auth.internal.signup.enabled,
-            "allow_pw_rest": config.auth.internal.signup.enabled
-        }
-        return make_api_response(data, msg, 401)
-    else:
-        resp = redirect(redirect_helper(f"/login.html?next={quote(request.full_path)}"))
-        resp.set_cookie('next_url', request.full_path)
-        resp.delete_cookie("XSRF-TOKEN")
-        return resp
+    data = {
+        "oauth_providers": [name for name, p in config.auth.oauth.providers.items()
+                            if p['client_id'] and p['client_secret']],
+        "allow_userpass_login": config.auth.ldap.enabled or config.auth.internal.enabled,
+        "allow_signup": config.auth.internal.signup.enabled,
+        "allow_pw_rest": config.auth.internal.signup.enabled
+    }
+    return make_api_response(data, msg, 401)
 
 
 @errors.app_errorhandler(403)
@@ -72,41 +57,26 @@ def handle_403(e):
 
         log_with_traceback(AUDIT_LOG, trace, f"Access Denied. (U:{uname} - IP:{ip}) [{error_message}]")
 
-    if request.path.startswith("/api/"):
-        config_block = {
-            "auth": {
-                "allow_2fa": config.auth.allow_2fa,
-                "allow_apikeys": config.auth.allow_apikeys,
-                "allow_security_tokens": config.auth.allow_security_tokens,
-                },
-            "ui": {
-                "allow_url_submissions": config.ui.allow_url_submissions,
-                "read_only": config.ui.read_only,
-                "tos": config.ui.tos not in [None, ""],
-                "tos_lockout": config.ui.tos_lockout,
-                "tos_lockout_notify": config.ui.tos_lockout_notify not in [None, []]
-                }
-        }
-        return make_api_response(config_block, "Access Denied (%s) [%s]" % (request.path, error_message), 403)
-    else:
-        if error_message.startswith("User") and str(e).endswith("is disabled"):
-            return render_template('403e.html', exception=error_message,
-                                   email=config.ui.email or "",
-                                   notified=config.ui.tos_lockout and config.ui.tos_lockout_notify), 403
-        else:
-            return render_template('403.html', exception=error_message), 403
+    config_block = {
+        "auth": {
+            "allow_2fa": config.auth.allow_2fa,
+            "allow_apikeys": config.auth.allow_apikeys,
+            "allow_security_tokens": config.auth.allow_security_tokens,
+            },
+        "ui": {
+            "allow_url_submissions": config.ui.allow_url_submissions,
+            "read_only": config.ui.read_only,
+            "tos": config.ui.tos not in [None, ""],
+            "tos_lockout": config.ui.tos_lockout,
+            "tos_lockout_notify": config.ui.tos_lockout_notify not in [None, []]
+            }
+    }
+    return make_api_response(config_block, "Access Denied (%s) [%s]" % (request.path, error_message), 403)
 
 
 @errors.app_errorhandler(404)
 def handle_404(_):
-    if request.path.startswith("/api/"):
-        return make_api_response("", "Api does not exist (%s)" % request.path, 404)
-
-    else:
-        if AL_NEXT_ONLY:
-            return current_app.send_static_file('index.html')
-
-        return render_template('404.html', url=request.path), 404
+    return make_api_response("", "Api does not exist (%s)" % request.path, 404)
 
 
 @errors.app_errorhandler(500)
@@ -123,7 +93,4 @@ def handle_500(e):
     log_with_traceback(LOGGER, trace, "Exception", is_exception=True)
 
     message = ''.join(['\n'] + format_tb(exc_info()[2]) + ['%s: %s\n' % (oe.__class__.__name__, str(oe))]).rstrip('\n')
-    if request.path.startswith("/api/"):
-        return make_api_response("", message, 500)
-    else:
-        return render_template('500.html', exception=message), 500
+    return make_api_response("", message, 500)
