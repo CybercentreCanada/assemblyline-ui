59f02d3237df01491876b9623612761be8f761c4
Steve Garon <steve.garon@cyber.gc.ca>
2021-03-11 10:02:23 -0500

    Treat value error as invalid apikey errors

--- assemblyline_ui/security/apikey_auth.py
+++ assemblyline_ui/security/apikey_auth.py
@@ -9,11 +9,15 @@ def validate_apikey(username, apikey, storage):
     if config.auth.allow_apikeys and apikey:
         user_data = storage.user.get(username)
         if user_data:
-            name, apikey_password = apikey.split(":", 1)
-            key = user_data.apikeys.get(name, None)
-            if key is not None:
-                if verify_password(apikey_password, key.password):
-                    return username, key.acl
+            try:
+                name, apikey_password = apikey.split(":", 1)
+                key = user_data.apikeys.get(name, None)
+
+                if key is not None:
+                    if verify_password(apikey_password, key.password):
+                        return username, key.acl
+            except ValueError:
+                pass
 
         raise AuthenticationException("Invalid apikey")
 
