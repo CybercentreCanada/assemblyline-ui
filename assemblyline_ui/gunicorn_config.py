44bd0e840b18ba0c2db1ee8135731eb7e0aeb309
Steve Garon <steve.garon@cyber.gc.ca>
2021-04-12 12:45:35 +0000

    Restart workers after 1000 requests to limit memory creeping

--- assemblyline_ui/gunicorn_config.py
+++ assemblyline_ui/gunicorn_config.py
@@ -5,3 +5,4 @@ import multiprocessing
 bind = f":{int(env.get('PORT', 5000))}"
 workers = int(env.get('WORKERS', multiprocessing.cpu_count()))
 threads = int(env.get('THREADS', 4))
+max_requests = int(env.get('MAX_REQUESTS', '1000'))
