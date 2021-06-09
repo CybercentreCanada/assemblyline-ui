5c8274ac4660ce56722018b6e163f40bc9541ec4
Steve Garon <steve.garon@cyber.gc.ca>
2021-06-09 14:40:08 +0000

    Added a add_update_many function for hashes

--- assemblyline_ui/api/v4/safelist.py
+++ assemblyline_ui/api/v4/safelist.py
@@ -11,8 +11,8 @@ safelist_api = make_subapi_blueprint(SUB_API, api_version=4)
 safelist_api._doc = "Perform operations on safelisted hashes"
 
 
-@safelist_api.route("/<qhash>/", methods=["PUT"])
-@api_login(require_type=['user', 'signature_importer'])
+@safelist_api.route("/<qhash>/", methods=["PUT", "POST"])
+@api_login(require_type=['user', 'signature_importer'], allow_readonly=False, required_priv=["W"])
 def add_or_update(qhash, **kwargs):
     """
     Add a hash in the safelist if it does not exist or update its list of sources if it does
@@ -51,7 +51,7 @@ def add_or_update(qhash, **kwargs):
      "op": "add"              # Was it added to the system or updated
     }
     """
-    # Validate hash lenght
+    # Validate hash length
     if len(qhash) not in [64, 40, 32]:
         return make_api_response(None, "Invalid hash length", 400)
 
@@ -133,8 +133,88 @@ def add_or_update(qhash, **kwargs):
                 return make_api_response({}, f"Invalid data provided: {str(e)}", 400)
 
 
+@safelist_api.route("/add_update_many/", methods=["POST", "PUT"])
+@api_login(audit=False, required_priv=['W'], allow_readonly=False, require_type=['signature_importer'])
+def add_update_many_hashes(**_):
+    """
+    Add or Update a list of the safe hashes
+
+    Variables:
+    None
+
+    Arguments:
+    None
+
+    Data Block (REQUIRED):
+    [                             # List of Safe hash blocks
+     {
+      "classification": "TLP:W",      # Classification of the file (default: TLP:W) - Optional
+      "fileinfo": {                   # Information about the file - Optional
+        "md5": "123...321",             # MD5 hash of the file
+        "sha1": "1234...4321",          # SHA1 hash of the file
+        "sha256": "12345....54321",     # SHA256 of the file (default: sha256 variable)
+        "size": 12345,                  # Size of the file
+        "type": "document/text"},       # Type of the file
+      "sources": [                    # List of sources for why the file is safelisted, dedupped on name - Required
+        {"name": "NSRL",                # Name of external source or user who safelisted it - Required
+         "reason": [                    # List of reasons why the source is safelisted - Required
+           "Found as test.txt on default windows 10 CD",
+           "Found as install.txt on default windows XP CD"
+         ],
+         "type": "external"},           # Type or source (external or user) - Required
+        {"name": "admin",
+         "reason": ["We've seen this file many times and it leads to False positives"],
+         "type": "user"}
+      ]
+     }
+     ...
+    ]
+
+    Result example:
+    {"success": 23,                # Number of hashes that succeeded
+     "errors": [],                 # List of hashes that failed
+     "skipped": [],                # List of skipped hashes, they already exist
+    """
+    data = request.json
+
+    if not isinstance(data, list):
+        return make_api_response("", "Could not get the list of hashes", 400)
+
+    new_data = {}
+    for hash_data in data:
+        hash_data.setdefault('classification', CLASSIFICATION.UNRESTRICTED)
+        fileinfo = hash_data.get('fileinfo', {})
+        key = fileinfo.get('sha256', fileinfo.get('sha1', fileinfo.get('md5', None)))
+        if not key:
+            return make_api_response("", f"Invalid hash block: {str(hash_data)}", 400)
+        new_data[key] = hash_data
+
+    old_data = STORAGE.safelist.multiget(list(new_data.keys()), as_dictionary=True, as_obj=False,
+                                         error_on_missing=False)
+
+    # Test signature names
+    plan = STORAGE.safelist.get_bulk_plan()
+    for key, val in new_data.items():
+        old_val = old_data.get(key, {'classification': CLASSIFICATION.UNRESTRICTED, 'fileinfo': {}, 'sources': []})
+        old_val['classification'] = CLASSIFICATION.max_classification(
+            old_val['classification'], val['classification'])
+        old_val['updated'] = now_as_iso()
+        old_val['fileinfo'].update(val['fileinfo'])
+        for source in val['sources']:
+            if source not in old_val['sources']:
+                old_val['sources'].append(source)
+
+        plan.add_upsert_operation(key, old_val)
+
+    if not plan.empty:
+        res = STORAGE.safelist.bulk(plan)
+        return make_api_response({"success": len(res['items']), "errors": res['errors']})
+
+    return make_api_response({"success": 0, "errors": []})
+
+
 @safelist_api.route("/<qhash>/", methods=["GET"])
-@api_login()
+@api_login(required_priv=["R"])
 def exists(qhash, **kwargs):
     """
     Check if a hash exists in the safelist.
@@ -165,7 +245,7 @@ def exists(qhash, **kwargs):
 
 
 @safelist_api.route("/<qhash>/", methods=["DELETE"])
-@api_login()
+@api_login(allow_readonly=False)
 def delete(qhash, **_):
     """
     Delete a hash from the safelist
