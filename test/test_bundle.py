93852bbddae7141aef8caa823607797d08b84ce4
Steve Garon <steve.garon@cyber.gc.ca>
2022-03-08 18:49:01 +0000

    Fix tests

--- test/test_bundle.py
+++ test/test_bundle.py
@@ -1,9 +1,11 @@
+
 import pytest
 import random
 
 from cart import is_cart
 from conftest import get_api_data
 
+from assemblyline.common import forge
 from assemblyline.common.bundling import create_bundle
 from assemblyline.odm.random_data import create_users, wipe_users, create_submission, wipe_submissions
 from assemblyline.odm.models.alert import Alert
@@ -14,6 +16,7 @@ ALERT_ID = "test_alert_id_ui"
 
 @pytest.fixture(scope="module")
 def datastore(datastore_connection, filestore):
+    classification = forge.get_classification()
     try:
         create_users(datastore_connection)
         submission = create_submission(datastore_connection, filestore)
@@ -32,7 +35,7 @@ def datastore(datastore_connection, filestore):
 def test_alert_create_bundle(datastore, login_session):
     _, session, host = login_session
 
-    resp = get_api_data(session, f"{host}/api/v4/bundle/{ALERT_ID}/", raw=True)
+    resp = get_api_data(session, f"{host}/api/v4/bundle/{ALERT_ID}/?use_alert", raw=True)
     assert is_cart(resp[:256])
 
 
@@ -43,12 +46,12 @@ def test_alert_import_bundle(datastore, login_session, filestore):
 
     # Create a temporary bundle
     alert = ds.alert.get_if_exists(ALERT_ID, as_obj=False)
-    submission = ds.submission.get_if_exists(alert['sid'])
+    submission = ds.submission.get_if_exists(alert['sid'], as_obj=False)
     bundle_file = create_bundle(ALERT_ID, working_dir='/tmp/bundle', use_alert=True)
 
     # Delete associated alert and submission
     ds.alert.delete(ALERT_ID)
-    ds.delete_submission_tree(submission['sid'], transport=filestore)
+    ds.delete_submission_tree(alert['sid'], transport=filestore)
     ds.alert.commit()
     ds.error.commit()
     ds.file.commit()
@@ -60,7 +63,9 @@ def test_alert_import_bundle(datastore, login_session, filestore):
         assert resp['success']
 
         ds.submission.commit()
-        assert submission == random.choice(ds.submission.search('id:*', rows=100, as_obj=False)['items'])
+        new_submission = ds.submission.get_if_exists(alert['sid'], as_obj=False)
+        assert new_submission['sid'] == alert['sid']
+        assert 'bundle.source' in new_submission['metadata']
 
         new_alert = ds.alert.get_if_exists(ALERT_ID, as_obj=False)
         assert new_alert['alert_id'] == ALERT_ID
