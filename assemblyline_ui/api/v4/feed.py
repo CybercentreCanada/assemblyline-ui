2f08f6724b74d03de6afde1da299e906c59201e3
Steve Garon <steve.garon@cyber.gc.ca>
2022-10-25 18:14:48 +0000

    Fetch all feeds from config

--- assemblyline_ui/api/v4/feed.py
+++ assemblyline_ui/api/v4/feed.py
@@ -1,14 +1,14 @@
 import requests
 
 from assemblyline_ui.api.base import api_login, make_api_response, make_subapi_blueprint
-
+from assemblyline_ui.config import config
 
 SUB_API = 'feed'
 feed_api = make_subapi_blueprint(SUB_API, api_version=4)
 feed_api._doc = "Fetch feed to provided link"
 
 
-@feed_api.route("/<url>/", methods=["GET"])
+@feed_api.route("/", methods=["GET"])
 @api_login(required_priv=['R'])
 def get_feed_data(url, **kwargs):
     """
@@ -26,7 +26,11 @@ def get_feed_data(url, **kwargs):
     Result example:
     <XML DATA>
     """
+    data = {}
     try:
-        return make_api_response(requests.get(url))
+        for feed in config.ui.rss_feeds:
+            data[feed] = requests.get(feed).content
+
+        return make_api_response(data)
     except Exception as e:
         return make_api_response(None, f"An exception occured while fetching the feed: {e}", 400)
