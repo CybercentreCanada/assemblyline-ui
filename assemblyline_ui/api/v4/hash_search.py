f71fa9edc0b4d49270cac09a6b997029ef490e02
cccs-ml <127332022+cccs-ml@users.noreply.github.com>
2023-06-16 15:04:13 +0000

    merge external hash_serach into existing endpoint

--- assemblyline_ui/api/v4/hash_search.py
+++ assemblyline_ui/api/v4/hash_search.py
@@ -1,9 +1,10 @@
 import concurrent.futures
+import copy
 import os
 import re
 
 from flask import request
-from requests import Session, exceptions
+from requests import Session
 
 from assemblyline.common.importing import load_module_by_path
 from assemblyline.odm.models.user import ROLES
@@ -42,6 +43,11 @@ def create_query_datasource(ds):
     return query_datasource
 
 
+# external sources should be refereced with an `x.` prefix/namespace in this module to avoid name collisions with
+# existing internal data sources
+external_sources = copy.deepcopy(getattr(config.ui, "external_sources", []))
+for s in external_sources:
+    s.name = f"x.{s.name}"
 sources = {}
 # noinspection PyBroadException
 try:
@@ -98,12 +104,13 @@ def get_external_details(
             ]
     }
     """
+    sname = source.name.removeprefix("x.")
     result = {"error": "", "items": []}
-    if hash_type not in all_supported_tags.get(source.name, {}):
+    if hash_type not in all_supported_tags.get(sname, {}):
         result["error"] = NOT_SUPPORTED
         return result
     # check the query against the max supported classification of the hash type in the external system
-    hash_type_classif = all_supported_tags[source.name][hash_type]
+    hash_type_classif = all_supported_tags[sname][hash_type]
     if not Classification.is_accessible(user["classification"], hash_type_classif):
         result["error"] = NOT_SUPPORTED
         return result
@@ -149,27 +156,30 @@ def get_external_details(
     return result
 
 
-@hash_search_api.route("/external/<path:file_hash>/", methods=["GET"])
-@api_login(require_role=[ROLES.external_query])
-def search_external(file_hash: str, **kwargs):
+# noinspection PyUnusedLocal
+@hash_search_api.route("/<file_hash>/", methods=["GET"])
+@api_login(require_role=[ROLES.alert_view, ROLES.submission_view, ROLES.external_query])
+def search_hash(file_hash, *args, **kwargs):
     """
     Search for a hash in multiple data sources as configured in the seed.
 
     Variables:
-    file_hash   => Hash to search in the external data sources
+    file_hash   => Hash to search in the multiple data sources
+                   [MD5, SHA1, SHA256, SSDEEP, TLSH]
 
     Arguments:(optional)
-    sources          => | separated list of data sources
+    db               => | separated list of data sources, external sources are prefixed with `x.`
     classification   => Classification of the tag [Default: minimum configured classification]
-    max_timeout     => Maximum execution time for the call in seconds [Default: 3 seconds]
-    limit           => limit the amount of returned results counted per source [Default: 500]
+    max_timeout      => Maximum execution time for the call in seconds [Default: 3]
+    limit            => limit the amount of returned results counted per source [Default: 500]
 
     Data Block:
     None
 
     API call examples:
     /api/v4/hash_search/
-    /api/v4/hash_search/123456...654321/?db=virustotal|al
+    /api/v4/hash_search/123456...654321/?db=nsrl|al
+    /api/v4/hash_search/123456...654321/?db=nsrl|al|x.virustotal
 
     Result example:
     {                           # Dictionary of:
@@ -186,48 +196,64 @@ def search_external(file_hash: str, **kwargs):
         ...
     }
     """
-    user = kwargs["user"]
-    query_sources = request.args.get("sources")
-    if query_sources:
-        query_sources = query_sources.split("|")
-    max_timeout = request.args.get("max_timeout", "3")
+    user = kwargs['user']
+    submitted_hash_type = next((x for x, y in HASH_MAP.items() if y.match(file_hash)), None)
+    if not submitted_hash_type:
+        return make_api_response("", f"Invalid hash. This API only supports {', '.join(HASH_MAP.keys())}.", 400)
+
+    hash_classification = request.args.get("classification", Classification.UNRESTRICTED)
     limit = request.args.get("limit", "500")
+    max_timeout = request.args.get('max_timeout', "3")
     # noinspection PyBroadException
     try:
         max_timeout = float(max_timeout)
     except Exception:
         max_timeout = 3.0
 
-    hash_type = next((x for x, y in HASH_MAP.items() if y.match(file_hash)), None)
-
-    if not hash_type:
-        return make_api_response("", f"Invalid hash. This API only supports {', '.join(HASH_MAP.keys())}.", 400)
-
-    hash_classification = request.args.get("classification", Classification.UNRESTRICTED)
+    db_list = []
+    ext_list = []
+    db = request.args.get('db', None)
+    if db:
+        for s in db.split("|"):
+            if s.startswith("x."):
+                ext_list.append(s)
+            elif hash_type(file_hash) != "invalid":
+                # internal db queries only support subset of hashes
+                db_list.append(s)
+    else:
+        db_list = sources.keys()
+        ext_list = [s.name for s in external_sources]
 
     # validate what sources the user is allowed to submit requests to.
     # this must first be checked against what systems the user is allowed to see
     # additional file hash level checking is then done later to provide feedback to user
-    available_sources = [
-        x for x in getattr(config.ui, "external_sources", [])
-        if Classification.is_accessible(user["classification"], x.classification)
-        and (not query_sources or x.name in query_sources)
+    ext = [
+        x for x in external_sources
+        if x.name in ext_list and Classification.is_accessible(user["classification"], x.classification)
     ]
 
-    with concurrent.futures.ThreadPoolExecutor(min(len(available_sources) + 1, os.cpu_count() + 4)) as executor:
+    total_sources = len(ext) + len(db_list)
+    with concurrent.futures.ThreadPoolExecutor(min(total_sources + 1, os.cpu_count() + 4)) as executor:
+        # create searches for external sources
         future_searches = {
             executor.submit(
                 get_external_details,
                 user=user,
                 source=source,
-                hash_type=hash_type,
+                hash_type=submitted_hash_type,
                 file_hash=file_hash,
                 hash_classification=hash_classification,
                 timeout=max(0, max_timeout - 0.5),
                 limit=limit,
             ): source.name
-            for source in available_sources
+            for source in ext
         }
+
+        # create searches for internal sources
+        future_searches.update({
+            executor.submit(sources[db], file_hash.lower(), user): db for db in db_list
+        })
+
         results = {
             future_searches[future]: future.result()
             for future in concurrent.futures.as_completed(future_searches, timeout=max_timeout)
@@ -259,78 +285,6 @@ def search_external(file_hash: str, **kwargs):
 
 
 # noinspection PyUnusedLocal
-@hash_search_api.route("/<file_hash>/", methods=["GET"])
-@api_login(require_role=[ROLES.alert_view, ROLES.submission_view])
-def search_hash(file_hash, *args, **kwargs):
-    """
-    Search for a hash in multiple data sources as configured in the seed.
-
-    Variables:
-    file_hash   => Hash to search in the multiple data sources
-                   [MD5, SHA1 or SHA256]
-
-    Arguments:(optional)
-    db          => | separated list of data sources
-    max_timeout => Maximum execution time for the call in seconds
-
-    Data Block:
-    None
-
-    API call examples:
-    /api/v4/hash_search/
-    /api/v4/hash_search/123456...654321/?db=nsrl|al
-
-    Result example:
-    {                           # Dictionary of:
-        "al": {                   # Data source queried
-          "error": null,            # Error message returned by data source
-          "items": [                # List of items found in the data source
-           {"confirmed": true,        # Is the maliciousness attribution confirmed or not
-            "data": {...}             # Raw data from the data source
-            "description": "",        # Description of the findings
-            "malicious": false},      # Is the file found malicious or not
-          ...
-          ]
-        },
-        ...
-    }
-    """
-
-    user = kwargs['user']
-    if hash_type(file_hash) == "invalid":
-        return make_api_response("", "Invalid hash. This API only supports MD5, SHA1 and SHA256.", 400)
-
-    db_list = []
-    invalid_sources = []
-    db = request.args.get('db', None)
-    if db:
-        db_list = db.split("|")
-        invalid_sources = []
-        for x in db_list:
-            if x not in sources:
-                invalid_sources.append(x)
-
-        for x in invalid_sources:
-            db_list.remove(x)
-
-    max_timeout = request.args.get('max_timeout', "2")
-    # noinspection PyBroadException
-    try:
-        max_timeout = float(max_timeout)
-    except Exception:
-        max_timeout = 2.0
-
-    if len(db_list) == 0 and len(invalid_sources) == 0:
-        db_list = sources.keys()
-
-    with concurrent.futures.ThreadPoolExecutor(len(db_list)) as executor:
-        res = {db: executor.submit(sources[db], file_hash.lower(), user) for db in db_list}
-
-    # TODO: Timeout part needs some love. Can't be done through dictionary comprehension.
-    return make_api_response({k: v.result(timeout=max_timeout) for k, v in res.items()})
-
-
-# noinspection PyUnusedLocal
 @hash_search_api.route("/list_data_sources/", methods=["GET"])
 @api_login(audit=False, require_role=[ROLES.alert_view, ROLES.submission_view])
 def list_data_sources(*args, **kwargs):
@@ -349,4 +303,6 @@ def list_data_sources(*args, **kwargs):
     Result example:
     [ <list of sources> ]
     """
-    return make_api_response(sorted(sources.keys()))
+    src = [s.name for s in external_sources]
+    src.extend(sources.keys())
+    return make_api_response(sorted(src))
