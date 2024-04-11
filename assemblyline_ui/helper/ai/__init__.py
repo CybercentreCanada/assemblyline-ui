3878bc44477a937e3ca095541b5c80e2f087ef48
Steve Garon <steve.garon@cyber.gc.ca>
2024-04-11 20:23:37 +0000

    Fix get_ai_agent function

--- assemblyline_ui/helper/ai/__init__.py
+++ assemblyline_ui/helper/ai/__init__.py
@@ -1,12 +1,12 @@
-from assemblyline.odm.models.config import AI as AIConfig
+from assemblyline.odm.models.config import Config
 from assemblyline_ui.helper.ai.cohere import CohereAgent
 from assemblyline_ui.helper.ai.openai import OpenAIAgent
 
 
-def get_ai_agent(config: AIConfig, logger):
-    if config.api_type == 'openai':
+def get_ai_agent(config: Config, logger):
+    if config.ui.ai.api_type == 'openai':
         return OpenAIAgent(config, logger)
-    elif config.api_type == 'cohere':
+    elif config.ui.ai.api_type == 'cohere':
         return CohereAgent(config, logger)
 
     raise ValueError(f'Invalid AI API type: {config.api_type}')
