ebe45ef4419d626e804a66a07f704a4a6f818de3
cccs-rs <62077998+cccs-rs@users.noreply.github.com>
2025-05-13 18:02:36 +0000

    Ensure changes made to test user are committed before proceeding with test

--- test/test_user.py
+++ test/test_user.py
@@ -335,6 +335,7 @@ def test_set_user_settings(datastore, login_session, allow_submission_customize)
         datastore.user.update(username, [(datastore.user.UPDATE_REMOVE, 'roles', 'submission_customize')])
         if 'submission_customize' in user['roles']:
             user['roles'].remove('submission_customize')
+    datastore.user.commit()
 
     uset = load_user_settings(user)
     uset['expand_min_score'] = 111
