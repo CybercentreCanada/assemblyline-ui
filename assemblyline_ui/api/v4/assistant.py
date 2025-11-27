2f9ffcba1e05b953b4f9910986986682464610c6
cccs-rs <62077998+cccs-rs@users.noreply.github.com>
2025-11-27 17:23:41 +0000

    Ensure users can't abuse integration with AI backends with irrelevant queries through AL as a proxy

--- assemblyline_ui/api/v4/assistant.py
+++ assemblyline_ui/api/v4/assistant.py
@@ -1,7 +1,8 @@
 from assemblyline.odm.models.user import ROLES
-from assemblyline_ui.api.base import api_login, make_api_response, make_subapi_blueprint
-from assemblyline_ui.config import AUDIT_LOG, AI_AGENT
 from flask import request
+
+from assemblyline_ui.api.base import api_login, make_api_response, make_subapi_blueprint
+from assemblyline_ui.config import AI_AGENT, AUDIT_LOG
 
 SUB_API = 'assistant'
 
@@ -58,6 +59,18 @@ def assistant_conversation(**kwargs):
 
     if not isinstance(messages, list):
         return make_api_response({}, "Input messages are not in the right format", 400)
+    if not messages:
+        return make_api_response({}, "No conversation messages provided", 400)
+
+    # Ensure AI integration with Assemblyline is not abused by enforcing the presence of a system message
+    first_message = messages[0]
+    if first_message['role'] != 'system' and first_message['content'] != AI_AGENT.config.ui.ai_backends.function_params.assistant.system_message:
+        # First message must be a system prompt to ensure proper context
+        # Sanitize the rest of the conversation to ensure there isn't anything overriding the system prompt
+        messages = [{
+            'role': 'system',
+            'content': AI_AGENT.config.ui.ai_backends.function_params.assistant.system_message,
+        }] + [msg for msg in messages if msg['role'] != 'system']
 
     for message in messages:
         if 'role' not in message or 'content' not in message:
