99293c626ec3c5776ee0d33617cef13f2df99841
cccs-rs <62077998+cccs-rs@users.noreply.github.com>
2025-12-15 21:25:47 +0000

    Update UI's submit API relative to allow extension of imported bundles

--- test/test_ui.py
+++ test/test_ui.py
@@ -1,40 +1,50 @@
 import hashlib
 import json
+import math
 import random
+from datetime import datetime
 from io import BytesIO
 
 import pytest
-from conftest import APIError, get_api_data
-
+from assemblyline.common.bundling import create_bundle
 from assemblyline.common.uid import get_random_id
 from assemblyline.odm.random_data import (
     create_services,
+    create_submission,
     create_users,
     wipe_services,
+    wipe_submissions,
     wipe_users,
 )
 from assemblyline.odm.randomizer import get_random_phrase
+from conftest import APIError, get_api_data
 
 
 @pytest.fixture(scope="module")
-def datastore(datastore_connection):
+def datastore(datastore_connection, filestore):
     try:
         create_users(datastore_connection)
         create_services(datastore_connection)
+        create_submission(datastore_connection, filestore)
         yield datastore_connection
     finally:
         wipe_users(datastore_connection)
         wipe_services(datastore_connection)
+        wipe_submissions(datastore_connection, filestore)
 
 
-def upload_file_flowjs(session, host):
+
+def upload_file_flowjs(session, host, data=None):
     total_chunk = random.randint(2, 5)
 
-    data = get_random_phrase(wmin=50, wmax=100)
-    if len(data) % total_chunk != 0:
-        data = data[: -(len(data) % total_chunk)]
+    if data is None:
+        # Insert random data if no data is provided
+        data = get_random_phrase(wmin=50, wmax=100)
 
-    chunk_size = int(len(data) / total_chunk)
+        if len(data) % total_chunk != 0:
+            data = data[: -(len(data) % total_chunk)]
+
+    chunk_size = math.ceil(len(data) / total_chunk)
     ui_id = get_random_id()
 
     counter = 0
@@ -69,7 +79,10 @@ def upload_file_flowjs(session, host):
                     "flowRelativePath": "/tmp/test/test.txt",
                     "flowTotalChunks": f"{total_chunk}",
                 }
-                bio = BytesIO(data[x * chunk_size : (x * chunk_size) + chunk_size].encode())
+                if isinstance(data, bytes):
+                    bio = BytesIO(data[x * chunk_size : (x * chunk_size) + chunk_size])
+                else:
+                    bio = BytesIO(data[x * chunk_size : (x * chunk_size) + chunk_size].encode())
                 resp = get_api_data(
                     session, f"{host}/api/v4/ui/flowjs/", method="POST", data=params, headers={}, files={"file": bio}
                 )
@@ -101,6 +114,34 @@ def test_ui_submission(datastore, login_session):
     assert submission.files[0].name == 'test.txt'
     assert submission.sid == resp['sid']
     assert submission.state == 'submitted'
+
+def test_ui_bundle_submission(datastore, login_session):
+    _, session, host = login_session
+
+    submission = datastore.submission.search("*", rows=1, fl="sid,expiry_ts", as_obj=False)['items'][0]
+    sid = submission['sid']
+    # Create a bundle file from a random submission
+    bundle_path = create_bundle(sid)
+
+    # Remove the submission created by create_bundle()
+    datastore.submission.delete(sid)
+
+    with open(bundle_path, "rb") as f:
+        ui_id, data = upload_file_flowjs(session, host, data=f.read())
+
+    ui_params = get_api_data(session, f"{host}/api/v4/user/settings/admin/")['submission_profiles']['static']
+    ui_params['filename'] = f'{sid}.al_bundle'
+    resp = get_api_data(session, f"{host}/api/v4/ui/start/{ui_id}/", method="POST", data=json.dumps(ui_params))
+    assert resp['started']
+
+    # The expectation is that the submission is restored from the bundle and the expiry_ts is extended
+    imported_submission = datastore.submission.get(sid, as_obj=False)
+
+    # Check to see if the results have been preserved upon import
+    assert imported_submission['results'] is not None
+
+    # Check that the expiry timestamp has been extended
+    assert datetime.fromisoformat(imported_submission['expiry_ts']) > datetime.fromisoformat(submission['expiry_ts'])
 
 
 def test_ui_submission_parameter(datastore, login_session):
